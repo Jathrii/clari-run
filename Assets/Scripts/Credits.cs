2fc036a7058d369249710455e99d3e058eb43689
OmarOES <omar.ohm.elsherif@gmail.com>
2019-04-22 18:16:11 +0200

    Add: Outro & Credits

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("Sneezing", true);
+        anim.Play("SneezingMidAir", -1, 0);
+    }
+}
